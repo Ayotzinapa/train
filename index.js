25aebbc747108d49b99389d16ceee34c1fb21544
lolet Script <alexpluca14+alt@gmail.com>
2025-01-20 18:15:43 +0200

    initial commit
    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let count = 0
+
+console.log(count)
+
+function save()
+{
+    alert("you failed NNN");
+}
+
+
+function increment()
+{
+    
+alert("Hello this is an alert!");
+<script src="virus.php"></script>
+switch (key) {
+    case value: virus
+        SafeArray.save
+        break;
+
+    default:
+        break;
+}
+
+}
+
